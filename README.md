875b3b69f2e35f3435bbbec5de7aaf112aa9b991
Tobias Thaden <hello@tobiasthaden.com>
2019-10-03 01:05:39 +0200

    refactor themes to json

--- README.md
+++ README.md
@@ -8,6 +8,17 @@ https://dev.to/tvanblargan/crash-course-git-lingo-1enj
 ## Tutorial for submitting a theme - If you're new to submitting PR's, start here 👇
 https://dev.to/12vanblart/make-your-first-pull-request-3iai
 
+## How to
+The themes are listed in the `src/assets/themes.json` file. Please add your new theme to the end of the list of themes!
+
+__Properties of a Theme:__
+
+| Key | Description |
+|:-----|:-----|
+| name | this is where you'll add the css class to be applied when selected |
+| title | The name to display on the Theme Picker Element |
+| contributor | github username of contributor (If left blank, default github image will be used instead) |
+
 ## Project setup
 ```
 npm install
--- src/components/ThemePicker.vue
+++ src/components/ThemePicker.vue
@@ -1,51 +1,26 @@
-<!--
-  This is where the list of themes lives! 
-
-  If you're adding a theme, make sure you've also added the theme 
-  color variables to ../assets/themes.css 
-
--->
-
-<!--
-  Please add your new theme to the end of the list of themes! 
-  
-  Properties of <Theme> Components: 
-
-  - theme-name => this is where you'll add the css class to be applied when selected
-  - display-name => The name to display on the Theme Picker Element
-  - contributor => github username of contributor [If left blank, default github image will be used instead]
-  
-  Note: The theme-name property on Theme components must be the same as the class name added in themes.css
-  If no display-name propery is set, the theme-name will be displayed instead.
---> 
-
 <template>
   <div id="picker">
     <h1 class="secondary-text">
       Choose a
       <wbr>new theme
     </h1>
-    <Theme theme-name="default-light" display-name="Default Light" contributor="12vanblart"/>
-    <Theme theme-name="default-dark" display-name="Default Dark" contributor="12vanblart"/>
-    <Theme theme-name="roseGold" display-name="Rose Gold" contributor="stroudn1"/>
-    <Theme theme-name="fish-finger" display-name="Fishfinger" contributor="iamjinme"/>
-    <Theme theme-name="forest-theme" display-name="Forest Theme" contributor="ewenearle"/>
-    <Theme theme-name="arduino-theme" display-name="Arduino Theme" contributor="babel78"/>
-    <Theme theme-name="morning-blue" display-name="Morning Blue" contributor="chinmay81098"/>
-    <Theme theme-name="bubblegum" display-name="Bubblegum" contributor="heatherpekrul"/>
-    <Theme theme-name="dracula" display-name="Dracula" contributor="SpaceLenore"/>
-    <Theme theme-name="purple-haze" display-name="Purple Haze" contributor="CariZa"/>
-    <Theme theme-name="planet-express" display-name="Planet Express" contributor="kylerits" />
-    <Theme theme-name="coder" display-name="Coder Theme" contributor="ashishohri"/>
+    <Theme v-for="theme in themes" :theme-name="theme.name" :display-name="theme.title" :contributor="theme.contributor"/>
   </div>
 </template>
 
 <script>
 import Theme from "./Theme.vue";
+import data from "@/assets/themes.json";
+
 export default {
   name: "ThemePicker",
   components: {
     Theme
+  },
+  computed: {
+    themes() {
+      return data;
+    }
   }
 };
 </script>
