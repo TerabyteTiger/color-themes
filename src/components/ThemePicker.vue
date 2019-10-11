75e1d576e4746e6c72f86f1c86ef276afbd07455
pmns <pietro.silva@pitang.com>
2019-10-11 14:03:29 -0300

    add theme Black yellow

--- src/components/ThemePicker.vue
+++ src/components/ThemePicker.vue
@@ -52,6 +52,7 @@
     <Theme theme-name="vintage-warm" display-name="Vintage Warm" contributor="prismprince" />
     <Theme theme-name="october-orange" display-name="October orange" contributor="CNname" />
     <Theme theme-name="red-theme" display-name="Red Theme" contributor="iamshadmirza" />
+    <theme theme-name="black-yellow" display-name="Black yellow" contributor="pipitorminator"/>
   </div>
 </template>
 
