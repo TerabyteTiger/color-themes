f045d1715d062c9b43977504c9167c71487f3c90
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-10-12 19:43:33 +0200

    added sunset red to theme

--- src/components/ThemePicker.vue
+++ src/components/ThemePicker.vue
@@ -54,6 +54,7 @@
       <Theme theme-name="october-orange" display-name="October orange" contributor="CNname" />
       <Theme theme-name="red-theme" display-name="Red Theme" contributor="iamshadmirza" />
       <Theme theme-name="black-yellow" display-name="Black yellow" contributor="pipitorminator"/>
+      <Theme theme-name="sunset-blue" display-name="Sunset blue" contributor="mannuelf" />
     </div>
   </div>
 </template>
