75f845449fefcd0960462497fef054d259b084cf
Renatto Oyague <renattoyague@gmail.com>
2019-10-13 12:59:11 -0500

    Add theme Skyblue-theme

--- src/components/ThemePicker.vue
+++ src/components/ThemePicker.vue
@@ -54,6 +54,7 @@
       <Theme theme-name="october-orange" display-name="October orange" contributor="CNname" />
       <Theme theme-name="red-theme" display-name="Red Theme" contributor="iamshadmirza" />
       <Theme theme-name="black-yellow" display-name="Black yellow" contributor="pipitorminator"/>
+      <Theme theme-name="skyblue-theme" display-name="skyblue-theme" contributor="LRK1696"/>
     </div>
   </div>
 </template>
