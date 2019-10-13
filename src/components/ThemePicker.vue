fafb9d28eeecb539f480ee478604fd1c494e2a31
Marco Túlio Terra <marcotulio.mtt@gmail.com>
2019-10-12 23:06:24 -0400

    added theme october green

--- src/components/ThemePicker.vue
+++ src/components/ThemePicker.vue
@@ -54,6 +54,7 @@
       <Theme theme-name="october-orange" display-name="October orange" contributor="CNname" />
       <Theme theme-name="red-theme" display-name="Red Theme" contributor="iamshadmirza" />
       <Theme theme-name="black-yellow" display-name="Black yellow" contributor="pipitorminator"/>
+      <Theme theme-name="october-green" display-name="October Green" contributor="marcotterra"/>
     </div>
   </div>
 </template>
