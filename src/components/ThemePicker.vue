5bce0792ef847e78f955796ae43155cd62d75db7
noviicee <cseanamika3@gmail.com>
2021-10-09 10:38:25 +0530

    added a new grinch theme

--- src/components/ThemePicker.vue
+++ src/components/ThemePicker.vue
@@ -26,6 +26,7 @@
       <wbr>new theme
     </h1>
     <div class="wrapper">
+      <Theme theme-name="grinch" display-name="Grinch" contributor="noviicee"/>
       <Theme theme-name="default-light" display-name="Default Light" contributor="12vanblart"/>
       <Theme theme-name="default-dark" display-name="Default Dark" contributor="12vanblart"/>
       <Theme theme-name="roseGold" display-name="Rose Gold" contributor="stroudn1"/>
